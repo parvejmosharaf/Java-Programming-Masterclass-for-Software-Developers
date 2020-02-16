5c77133d20d4aa29a8dcd2b09199c2de9854dd0d
parvej <parvej1114@yahoo.com>
2020-02-16 22:42:26 +0800

    float and double

---
+++ src/java_tim/var_dtype_operators/FloatAndDouble.java
@@ -0,0 +1,13 @@
+package java_tim.var_dtype_operators;
+
+public class FloatAndDouble {
+    public static void main(String[] args) {
+        int intValue = 5/2;
+        float floatValue = 5f / 3f;
+        double doubleValue = 5d / 3d;
+
+        System.out.println("Int value: " + intValue);
+        System.out.println("Float Value: " + floatValue);
+        System.out.println("Double value: "+ doubleValue);
+    }
+}
