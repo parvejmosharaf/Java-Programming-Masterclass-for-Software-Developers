6a134e3e35aefbf53c65db96c1f0624897665a9e
parvej <parvej1114@yahoo.com>
2020-02-16 22:02:11 +0800

    just did a basic program.

---
+++ src/java_tim/var_dtype_operators/BasicInt.java
@@ -0,0 +1,13 @@
+package java_tim.var_dtype_operators;
+
+public class BasicInt {
+    public static void main(String[] args) {
+
+        int firstNumber = (10 + 5) + (2 * 10);
+        int secondNumber = 12;
+        int thirdNumber = firstNumber * 2;
+        int total = firstNumber + secondNumber + thirdNumber;
+        int ans = 1000 - total;
+        System.out.println("Ans is: " + ans);
+    }
+}
