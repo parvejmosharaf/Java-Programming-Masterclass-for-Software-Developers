f73fca2c7a522c1143132eef47d9eec59063f1fb
parvej <parvej1114@yahoo.com>
2020-02-16 23:02:06 +0800

    bool and char

---
+++ src/java_tim/var_dtype_operators/CharAndBoolean.java
@@ -0,0 +1,15 @@
+package java_tim.var_dtype_operators;
+
+public class CharAndBoolean {
+    public static void main(String[] args) {
+        char myChar = '\u00A9';
+        System.out.println(myChar);
+
+        boolean myBool = false;
+        boolean isMale = true;
+
+        char qChar = '\u00AE';
+        System.out.println(qChar);
+
+    }
+}
