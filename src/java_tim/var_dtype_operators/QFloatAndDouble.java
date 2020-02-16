fbffccbd26ab6980568b62e4e0b487e14a8cdb63
parvej <parvej1114@yahoo.com>
2020-02-16 22:51:06 +0800

    Solve float and double

---
+++ src/java_tim/var_dtype_operators/QFloatAndDouble.java
@@ -0,0 +1,9 @@
+package java_tim.var_dtype_operators;
+
+public class QFloatAndDouble {
+    public static void main(String[] args) {
+        double pound = 7;
+        double kilograms = pound * .45359237;
+        System.out.println(pound + " pound = " + kilograms + " kg.");
+    }
+}
